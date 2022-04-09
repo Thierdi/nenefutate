c84d50e5c5fd3350d77de262daa06e8014a29b56
Thierdi <97932695+Thierdi@users.noreply.github.com>
2022-04-09 14:11:15 -0400

    added the api link

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
   const [username, setUsername] = useState("");
 
   useEffect(() => {
-    Axios.get("http://localhost:3001/getUsers").then((response) => {
+    Axios.get("https://nenefutate.herokuapp.com/getUsers").then((response) => {
       setListOfUsers(response.data);
     });
   }, []);
 
   const createUser = () => {
-    Axios.post("http://localhost:3001/createUser", {
+    Axios.post("https://nenefutate.herokuapp.com/createUser", {
       name,
       age,
       username,
